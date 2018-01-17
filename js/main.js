594b46a3eb068ce744acd48df5545ef60abb25b1
Andrew Henry <ajh145@pitt.edu>
2018-01-17 01:58:28 -0500

    Changed a lot

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
+
 (function(){
+
+
+//Used for on page loading
 anime({
     targets: '#heading',
     opacity: {
@@ -8,12 +12,45 @@ anime({
     },
     delay: 250,
     translateY: {
-        value: -50,
+        value: [50, 0],
         duration: 3000,
         easing: 'easeOutExpo'
       }
   });
 
+  anime({
+    targets: '#scroll-text',
+    opacity: {
+        value: 1,
+        duration: 3000,
+        easing: 'easeOutExpo'
+    },
+    delay: 2000
+  });
+
+  //Both for the same object
+  anime({
+    targets: '#scroll-icon',
+    opacity: {
+        value: 1,
+        duration: 3000,
+        easing: 'easeOutExpo'
+    },
+    delay: 2000
+  });
+
+  anime({
+    targets: '#scroll-icon',
+    translateY: {
+        value: 10,
+        duration: 800,
+      },
+    direction: 'alternate',
+    loop: true
+  });
+
+
+  
   anime({
     targets: '#slogan',
     opacity: {
@@ -23,11 +60,35 @@ anime({
     },
     delay: 1000,
     translateY: {
-        value: -50,
+        value: [50, 0],
         duration: 3000,
         easing: 'easeOutExpo'
       }
   });
 
-
 })();
+
+//Cache reference to window and animation items
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+
+function check_if_in_view() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+  
+    $.each($animation_elements, function() {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+  
+      //check to see if this current container is within viewport
+      if ((element_bottom_position >= window_top_position) &&
+          (element_top_position <= window_bottom_position)) {
+        $element.addClass('in-view');
+      } else {
+        $element.removeClass('in-view');
+      }
+    });
+  }
